5724d720509ae18be4e4126fb34e949ea733d1bb
andy0532 <andy0532@gmail.com>
2026-06-28 19:06:35 +0800

    chore: 清理调试文件

--- src/utils/ws-debug.ts
+++
@@ -1,8 +0,0 @@
-// Log incoming WebSocket message details
-// Append this to the DO's WebSocket message handler for debugging
-
-export function logIncomingMessage(data: string) {
-  const prefix = data.substring(0, 20)
-  const startsWith = data.substring(0, 3)
-  console.log('WS_MSG_IN: len=' + data.length + ' prefix=' + prefix + ' cg_?=' + (startsWith === 'cg_'))
-}
